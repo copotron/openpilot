124f3d010099a18a794b7ce5b8be6f5ed71e3627
Maruf Abbasi <kuasha@gmail.com>
2019-11-07 07:42:21 -0800

    Change API endpoint

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -30,11 +30,11 @@ class Api():
     return jwt.encode(payload, self.private_key, algorithm='RS256').decode('utf8')
 
 def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
-  backend = "https://api.commadotai.com/"
+  backend = "https://n3n25uskx1.execute-api.us-east-1.amazonaws.com/prod/openpilot/"
 
   headers = {}
   if access_token is not None:
-    headers['Authorization'] = "JWT "+access_token
+    headers['Authorization'] = "Bearer "+access_token
 
   headers['User-Agent'] = "openpilot-" + version
 
